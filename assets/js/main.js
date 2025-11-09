19cb34807be6f21374f2e9be2ced57ee316ff213
Emil Eriksson <98emil.eriksson@gmail.com>
2025-11-09 09:47:16 +0100

    add animation to hamburger button

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Hamburger menu
+const hamburgerButton = document.getElementById("hamburger-btn");
+
+hamburgerButton.addEventListener("click", () => {
+  hamburgerButton.classList.toggle("open");
+});
