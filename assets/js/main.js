91bdfc075cd177cecb8aa7d6c30d8141ee59789a
Emil Eriksson <98emil.eriksson@gmail.com>
2025-12-03 22:35:16 +0100

    add slide animation for mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // Hamburger menu
 const hamburgerButton = document.getElementById("hamburger-btn");
+const hamburgerNav = document.getElementById("mobile-nav");
 
 hamburgerButton.addEventListener("click", () => {
   hamburgerButton.classList.toggle("open");
+  hamburgerNav.classList.toggle("open");
 });
